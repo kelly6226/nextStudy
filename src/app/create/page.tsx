3e42c31cc0b70138fa206d683741d7f831fd0380
kelly6226 <kelly6226@naver.com>
2025-02-01 21:28:03 +0900

    feat(low): 새로운 요소 생성 api 연결

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
 export default function Create() {
-  return <div>create 메인 페이지입니다.</div>;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const title = (form.elements.namedItem("title") as HTMLInputElement).value;
+    const body = (form.elements.namedItem("body") as HTMLTextAreaElement).value;
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, body }),
+    };
+
+    fetch("http://localhost:9999/topics", options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        return console.log(result);
+      });
+  };
+
+  return (
+    <form onSubmit={(e) => onSubmit(e)}>
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
