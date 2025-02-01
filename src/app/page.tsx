1e20159c1a01fab9e8b29ca9c4b2ca79653f2402
kelly6226 <kelly6226@naver.com>
2025-02-01 19:23:13 +0900

    feat(low): front 패이지 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Image from "next/image";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <>
       <h1>Welcome</h1>
-      <div>
+      <Column>
         <p>Hello. I am making this web by using Next.js</p>
         <Image src="/next.svg" alt="next" width="100" height="50" />
-      </div>
+      </Column>
     </>
   );
 }
 
-// const Column = styled.div``;
+const Column = styled.div`
+  display: flex;
+  flex-direction: row;
+  grid-gap: 30px;
+`;
